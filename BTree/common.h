a0b84a5e6846442c3f8f0964c38c45f9e362636d
배규리 <32202018@dankook.ac.kr>
2022-06-10 23:13:52 +0900

    Create common.h

---
+++ BTree/common.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+#include<ctime>
+using namespace std;
+class Node
+{
+	int* keys;
+	int t;
+	Node** C;
+	int n;
+	bool l;
+public:
+	friend class BTree;
+	Node(int value, bool l);
+	Node* search(int value);
+	void insert(int value);
+	void split(int i, Node* y);
+	void traverse();
+};
+
+class BTree
+{
+	Node* root;
+	int t;
+public:
+	BTree(int value)
+	{
+		root = NULL;
+		t = value;
+	}
+	Node* search(int k)
+	{
+		return (root == NULL) ? NULL : root->search(k);
+	}
+	void insert(int k);
+	void traverse()
+	{
+		if (root != NULL) root->traverse();
+	}
+	
+	
+};
